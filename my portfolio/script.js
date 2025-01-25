4d2d615544804c8112a32012eb5abee3d0c42721
sonu kumar <krsonu002@gmail.com>
2025-01-25 22:00:08 +0530

    changes

--- my portfolio/script.js
+++ my portfolio/script.js
@@ -12,53 +12,47 @@ function opentab(tabname) {
     // }
     document.querySelector(`[onclick="opentab('${tabname}')"]`).classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
-
+    // Close the sidebar when a tab is clicked (for mobile view)
+    closemenu();
 }
 // side menu
 var sidemenu = document.getElementById("sidemenu");
 function openmenu() {
-    sidemenu.style.right = "0";
+    // sidemenu.style.right = "0";
+    sidemenu.classList.add("active");
 }
 function closemenu() {
-    sidemenu.style.right = "-200px";
+    // sidemenu.style.right = "-250px";
+    sidemenu.classList.remove("active");
 }
+document.addEventListener('click',function (event){
+    if(!sidemenu.contains(event.target)&& !event.target.matches('.fa-bars')){
+        closemenu();
+    }
+});
+// window.addEventListener('resize',function(){
+//     if(this.window.innerWidth>768){
+//         sidemenu.classList.remove("active");
+//         sidemenu.style.right="0";
+//     }
+//     else{
+//         sidemenu.style.right="-250px";
+//     }
+// });
 const scriptURL = 'https://script.google.com/macros/s/AKfycby2BFQZE1H0DD5mXG1EiXr-Hg7lzjZOtwHtrjgGHkfmWjdipQmkfS6rETWkMbx3Poj1/exec'
 const form = document.forms['submit-to-google-sheet']
-const msg=document.getElementById("msg")
+const msg = document.getElementById("msg")
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {msg.innerHTML="Message sent successfully"
-            setTimeout(function(){
-                msg.innerHTML=""
-            },5000)
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
             form.reset()
         })
         .catch(error => console.error('Error!', error.message))
 })
-// const certificates = [
-//     { img: "images/ibm cloud.png", pdf: "images\ibm cloud computing.pdf" },
-//     { img: "images/ibm html css.png", pdf: "images/intro to html css.pdf" },
-//     // Add more certificates here
-//   ];
-  
-//   const container = document.getElementById("certificates");
-  
-//   certificates.forEach(cert => {
-//     const certDiv = document.createElement("div");
-//     certDiv.className = "certificate";
-  
-//     const img = document.createElement("img");
-//     img.src = cert.img;
-//     img.alt = "Certificate Sample";
-  
-//     const link = document.createElement("a");
-//     link.href = cert.pdf;
-//     link.target = "_blank";
-//     link.textContent = "View Full Certificate";
-  
-//     certDiv.appendChild(img);
-//     certDiv.appendChild(link);
-//     container.appendChild(certDiv);
-//   });
-  
+
+
